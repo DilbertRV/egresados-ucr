48680e903b457254b34f043e33649a0f5fa83446
dilbertrv <gearxdtrece@gmail.com>
2023-11-08 21:40:41 -0600

    Add data and schema files for tasks

---
+++ src/data/data.jsx
@@ -0,0 +1,162 @@
+import {
+  ArrowDown,
+  ArrowRight,
+  ArrowUp,
+  CheckCircle2,
+  Circle,
+  XCircle,
+  FileQuestion,
+  Timer,
+} from "lucide-react"
+
+export const year_of_graduation = [
+  {
+    value: 2016,
+    label: "2016",
+  },
+  {
+    value: 2017,
+    label: "2017",
+  },
+  {
+    value: 2018,
+    label: "2018",
+  },
+  {
+    value: 2019,
+    label: "2019",
+  },
+  {
+    value: 2020,
+    label: "2020",
+  },
+  {
+    value: 2021,
+    label: "2021",
+  },
+  {
+    value: 2022,
+    label: "2022",
+  },
+  {
+    value: 2023,
+    label: "2023",
+  },
+];
+
+export const titles = [
+  {
+    value: "maestria",
+    label: "Maestría",
+  },
+  {
+    value: "licenciatura",
+    label: "Licenciatura",
+  },
+  {
+    value: "bachillerato",
+    label: "Bachillerato",
+  },
+  {
+    value: "doctorado",
+    label: "Doctorado",
+  },
+];
+
+export const empresas = [
+  {
+    value: "costa_rica",
+    label: "Costa Rica",
+  },
+  {
+    value: "estados_unidos",
+    label: "Estados Unidos",
+  },
+  {
+    value: "canada",
+    label: "Canadá",
+  },
+  {
+    value: "espana",
+    label: "España",
+  },
+  {
+    value: "alemania",
+    label: "Alemania",
+  },
+  {
+    value: "francia",
+    label: "Francia",
+  },
+  {
+    value: "inglaterra",
+    label: "Inglaterra",
+  },
+  {
+    value: "italia",
+    label: "Italia",
+  },
+  {
+    value: "suiza",
+    label: "Suiza",
+  },
+  {
+    value: "mexico",
+    label: "México",
+  },
+  {
+    value: "panama",
+    label: "Panamá",
+  },
+  {
+    value: "colombia",
+    label: "Colombia",
+  },
+  {
+    value: "brasil",
+    label: "Brasil",
+  },
+  {
+    value: "argentina",
+    label: "Argentina",
+  },
+  {
+    value: "chile",
+    label: "Chile",
+  },
+];
+
+export const egresos = [
+  {
+    value: 2016,
+    label: "2016",
+  },
+  {
+    value: 2017,
+    label: "2017",
+  },
+  {
+    value: 2018,
+    label: "2018",
+  },
+  {
+    value: 2019,
+    label: "2019",
+  },
+  {
+    value: 2020,
+    label: "2020",
+  },
+  {
+    value: 2021,
+    label: "2021",
+  },
+  {
+    value: 2022,
+    label: "2022",
+  },
+  {
+    value: 2023,
+    label: "2023",
+  },
+];
