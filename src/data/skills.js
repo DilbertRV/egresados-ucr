6774d70dbc9cd9e129d119d936779751e4a15984
dilbertrv <gearxdtrece@gmail.com>
2023-12-04 04:11:23 -0600

    Add skills data to project

---
+++ src/data/skills.js
@@ -0,0 +1,81 @@
+export const softSkills = [
+  {
+    id: "comunicacion",
+    nombre: "Comunicación",
+  },
+  {
+    id: "liderazgo",
+    nombre: "Liderazgo",
+  },
+  {
+    id: "resolucion_de_problemas",
+    nombre: "Resolución de problemas",
+  },
+  {
+    id: "gestion_del_tiempo",
+    nombre: "Gestión del tiempo",
+  },
+  {
+    id: "toma_de_decisiones",
+    nombre: "Toma de decisiones",
+  },
+  {
+    id: "Trabajo_en_equipo",
+    nombre: "Trabajo en equipo",
+  },
+];
+
+export const skills = [
+  {
+    id: 1,
+    carrera: "Informática Empresarial",
+    habilidades: [
+      {
+        id: "programacion",
+        nombre: "Programación",
+      },
+      {
+        id: "bases_de_datos",
+        nombre: "Bases de datos",
+      },
+      {
+        id: "redes",
+        nombre: "Redes",
+      },
+      {
+        id: "analisis_de_datos",
+        nombre: "Análisis de datos",
+      },
+      {
+        id: "seguridad_informatica",
+        nombre: "Seguridad informática",
+      },
+    ],
+  },
+  {
+    id: 2,
+    carrera: "Dirección de Empresas",
+    habilidades: [
+      {
+        id: "contabilidad",
+        nombre: "Contabilidad",
+      },
+      {
+        id: "recursos_humanos",
+        nombre: "Recursos Humanos",
+      },
+      {
+        id: "finanzas",
+        nombre: "Finanzas",
+      },
+      {
+        id: "mercadeo",
+        nombre: "Mercadeo",
+      },
+      {
+        id: "tecnoologias_de_informacion",
+        nombre: "Tecnologías de Información",
+      },
+    ],
+  },
+];
